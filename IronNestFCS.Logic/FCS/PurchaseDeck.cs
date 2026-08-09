3d541e691e58263c7d06e1acbd2af546ef0ff291
hyson <499525549@qq.com>
2026-08-09 13:07:18 +0800

    fix: map PCLM purchase card to PLCM shell type

--- IronNestFCS.Logic/FCS/PurchaseDeck.cs
+++ IronNestFCS.Logic/FCS/PurchaseDeck.cs
@@ -17,7 +17,10 @@ public class PurchaseDeck {
         foreach (var card in cards) {
             MelonLogger.Msg($"[FCS] PurchaseDeck: Found card {card.CurrentDefinition.ID}");
             if (TryParse(
-                    card.CurrentDefinition.ID.Replace("SMOKE", "SMK").Replace("Shell", ""),
+                    card.CurrentDefinition.ID
+                        .Replace("SMOKE", "SMK")
+                        .Replace("PCLM", "PLCM")
+                        .Replace("Shell", ""),
                     out BulletType type
                 )) {
                 bulletCards[type] = card.transform;
