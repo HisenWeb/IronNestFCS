ad1964cc8c04f5e7a1614cf70cfe8d21b2fa0191
hyson <499525549@qq.com>
2026-08-11 04:33:35 +0800

    docs: mark Smart planner source attribution

--- IronNestFCS.Logic/Scheduling/FirePlanner.cs
+++ IronNestFCS.Logic/Scheduling/FirePlanner.cs
@@ -1,3 +1,7 @@
+// Smart fork modifications Copyright (c) 2026 HisenWeb
+// Based on IronNestFCS by svr2kos2
+// SPDX-License-Identifier: MIT
+
 using System.Collections;
 using Il2Cpp;
 using IronNestFCS.Abstractions;
